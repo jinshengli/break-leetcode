8457ce45a4878573f3fe632bc098a321705b059c
jinshengli <1515026890@qq.com>
2020-07-18 18:44:56 +0800

    doublepointer

--- src/doublepointer/FindClosest_m1711.java
+++ src/doublepointer/FindClosest_m1711.java
@@ -3,6 +3,43 @@ package doublepointer;
 /**
  * create by ljs on 2020/7/14 10:07
  * description:
+ *
+ *
+ *  面试题 17.11. 单词距离
+ *
+ *
  */
-public class FindClosest {
+public class FindClosest_m1711 {
+
+
+    public int findClosest(String[] words, String word1, String word2) {
+
+
+        int index1 = -1;
+        int index2 = -1;
+        int minRes = Integer.MAX_VALUE;
+
+        for ( int i = 0; i < words.length; ++i ) {
+
+            if ( word1.equals(words[i]) ) {
+                index1 = i;
+            }
+
+            if ( word2.equals(words[i]) ) {
+                index2 = i;
+            }
+
+            if ( index1 != -1 && index2 != -1 ) {
+                minRes = Math.min(minRes, Math.abs(index1-index2));
+            }
+
+        }
+
+
+        return minRes;
+
+
+    }
+
+
 }
---
+++ src/doublepointer/README.md
@@ -0,0 +1,7 @@
+
+
+##### 双指针
+
+
+1.  面试题 17.11. 单词距离
+https://leetcode-cn.com/problems/find-closest-lcci/
