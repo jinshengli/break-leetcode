f22713f5e9ab26924285e4c8be89972882f3ce6d
jinshengli <1515026890@qq.com>
2020-07-18 18:43:49 +0800

    breath_first_search

--- src/breath_first_search/LargestValues_515.java
+++ src/breath_first_search/LargestValues_515.java
@@ -1,8 +1,60 @@
 package breath_first_search;
 
+import binarytree.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * create by ljs on 2020/7/17 19:43
  * description:
+ *
+ *  515. 在每个树行中找最大值
+ *
+ *           1
+ *          / \
+ *         3   2
+ *        / \   \
+ *       5   3   9
+ *
+ *       [1,3,9]
+ *
+ *
  */
+
 public class LargestValues_515 {
+
+    public List<Integer> largestValues(TreeNode root) {
+
+        if ( root == null ) {
+            return new ArrayList<>();
+        }
+
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        LinkedList<Integer> result = new LinkedList<>();
+        while ( !queue.isEmpty() ) {
+
+            int size = queue.size();
+            int max = Integer.MIN_VALUE;
+            for ( int i =0; i < size; ++i ) {
+
+                TreeNode poll = queue.poll();
+                max = Math.max(poll.val, max);
+                if ( poll.left != null )
+                    queue.offer(poll.left);
+
+                if ( poll.right != null )
+                    queue.offer(poll.right);
+            }
+
+            // 找到一层的最大值。
+            result.add(max);
+        }
+
+        return result;
+    }
+
+
 }
--- src/breath_first_search/levelOrder_102.java
+++ src/breath_first_search/levelOrder_102.java
@@ -1,8 +1,47 @@
 package breath_first_search;
 
+import binarytree.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * create by ljs on 2020/7/17 19:54
  * description:
  */
 public class levelOrder_102 {
+
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+
+        List<List<Integer>> result = new ArrayList<>();
+        LinkedList<TreeNode> queue = new LinkedList<>();
+
+        if ( root != null )
+            queue.offer(root);
+
+        while ( !queue.isEmpty() ) {
+
+            int size = queue.size();
+            ArrayList<Integer> level = new ArrayList<>();
+            for ( int i=0; i < size; ++i ) {
+
+                TreeNode node = queue.poll();
+                level.add(node.val);
+                if ( node.left != null )
+                    queue.offer(node.left);
+                if ( node.right != null )
+                    queue.offer(node.right);
+            }
+
+            result.add(level); // 添加一层。
+
+        }
+
+        return result;
+    }
+
+
+
 }
