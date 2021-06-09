07a4a4fce17e65b594cf4eb0e1701e600891acad
lijinsheng <1515026890@qq.com>
2021-06-09 11:05:13 +0800

    删除链表节点的值

--- src/linklist/DeleteNode_offer18.java
+++ src/linklist/DeleteNode_offer18.java
@@ -3,6 +3,7 @@ package linklist;
 /**
  * create by ljs on 2020/7/11 8:17
  * description:
+ * 给定单向链表的头指针和一个要删除的节点的值，定义一个函数删除该节点。
  *
  *
  *  1. 要删除的节点在第一个位置
@@ -31,12 +32,12 @@ public class DeleteNode_offer18 {
         while ( cur != null ) {
             if ( cur.val == val ) {
                 if ( pre == null ) { // 删除第一个节点
-                    pre = cur.next;
-                    head = pre;
+                    cur = cur.next;
+                    head = cur;
                 } else {    // 删除其它位置的一个节点
                     pre.next = cur.next;
+                    cur = cur.next;
                 }
-                cur = cur.next;
             } else {
                 pre = cur;
                 cur = cur.next;
@@ -44,4 +45,23 @@ public class DeleteNode_offer18 {
         }
         return head;
     }
+
+    /**
+     *  就删除一个val值的节点
+     * @param head
+     * @param val
+     * @return
+     */
+    public ListNode deleteNode01(ListNode head, int val) {
+        if (head == null) return head;
+        if (head.val == val) return head.next;
+        ListNode pre = head, cur = head.next;
+        while (cur != null && cur.val != val) {
+            pre = cur;
+            cur = cur.next;
+        }
+        if (cur != null) pre.next = cur.next;
+        return head;
+    }
+
 }
